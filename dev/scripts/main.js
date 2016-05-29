b9eb704209748977cd66b2850b50554fbd047349
Michele Prandina <michele@nevergiveapp.com>
2016-05-29 21:34:08 +0200

    Add jQuery and loadash to HTML in order to enable smootscroll

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,31 @@
 /**
  * Created by micheleprandina on 28/05/16.
  */
+
+// Guards against the function becoming an argument when two files with IIFEs are concatenated)
+;(function($) {
+    'use strict';
+
+    // Cache DOM
+    var theDocument = $('html, body');
+    var navTabItemAnchor = $('.nav-tab-item-anchor');
+
+    navTabItemAnchor.on('click', function(evnt) {
+
+        // Prevent default scroll
+        evnt.preventDefault();
+
+        // Cache this
+        var that = $(this);
+        var elemAttr = $(that.attr('href'));
+
+        theDocument.animate({
+            scrollTop: elemAttr.offset().top
+        }, 320);
+
+
+    });
+
+
+
+})(jQuery);
