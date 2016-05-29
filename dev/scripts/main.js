7c0bbb050baaf03116cc21bcec295e9362df01f7
Michele Prandina <michele@nevergiveapp.com>
2016-05-29 22:28:45 +0200

    Change single quotes with double quotes - lint

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -2,22 +2,21 @@
  * Created by micheleprandina on 28/05/16.
  */
 
-// Guards against the function becoming an argument when two files with IIFEs are concatenated)
-;(function($) {
-    'use strict';
+(function($) {
+    "use strict";
 
     // Cache DOM
-    var theDocument = $('html, body');
-    var navTabItemAnchor = $('.nav-tab-item-anchor');
+    var theDocument = $("html, body");
+    var navTabItemAnchor = $(".nav-tab-item-anchor");
 
-    navTabItemAnchor.on('click', function(evnt) {
+    navTabItemAnchor.on("click", function(evnt) {
 
         // Prevent default scroll
         evnt.preventDefault();
 
         // Cache this
         var that = $(this);
-        var elemAttr = $(that.attr('href'));
+        var elemAttr = $(that.attr("href"));
 
         theDocument.animate({
             scrollTop: elemAttr.offset().top
